99b8a4f7701a906510ad106c7fa971b805948915
IsaacMoris <isaac.moris9@gmail.com>
2021-11-02 00:41:53 +0200

    FFT

--- Algebra/FFT.cpp
+++ Algebra/FFT.cpp
@@ -1,62 +1,51 @@
 #include<iostream>
 #include <bits/stdc++.h>
 #define ll long long
-#define ld  double
+#define ld long double
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
 #define IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
-using cd = complex<double>;
-const double PI = acos(-1);
-void fft(vector<cd> & a, bool invert) {
-    int n = a.size();
-
-    for (int i = 1, j = 0; i < n; i++) {
-        int bit = n >> 1;
-        for (; j & bit; bit >>= 1)
-            j ^= bit;
-        j ^= bit;
-
-        if (i < j)
-            swap(a[i], a[j]);
+typedef complex<double> C;
+typedef vector<double> vd;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+void fft(vector<C>& a) {
+    int n = sz(a), L = 31 - __builtin_clz(n);
+    static vector<complex<long double>> R(2, 1);
+    static vector<C> rt(2, 1); // (^ 10% fas te r i f double)
+    for (static int k = 2; k < n; k *= 2) {
+        R.resize(n);
+        rt.resize(n);
+        auto x = polar(1.0L, acos(-1.0L) / k);
+        rep(i, k, 2 * k) rt[i] = R[i] = i & 1 ? R[i / 2] * x : R[i / 2];
     }
-
-    for (int len = 2; len <= n; len <<= 1) {
-        double ang = 2 * PI / len * (invert ? -1 : 1);
-        cd wlen(cos(ang), sin(ang));
-        for (int i = 0; i < n; i += len) {
-            cd w(1);
-            for (int j = 0; j < len / 2; j++) {
-                cd u = a[i + j], v = a[i + j + len / 2] * w;
-                a[i + j] = u + v;
-                a[i + j + len / 2] = u - v;
-                w *= wlen;
-            }
+    vi rev(n);
+    rep(i, 0, n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+    rep(i, 0, n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+    for (int k = 1; k < n; k *= 2)
+        for (int i = 0; i < n; i += 2 * k) rep(j, 0, k) {
+            C z = rt[j + k] * a[i + j + k]; //
+            a[i + j + k] = a[i + j] - z;
+            a[i + j] += z;
         }
-    }
-
-    if (invert) {
-        for (cd & x : a)
-            x /= n;
-    }
 }
-vector<ll> multiply(vector<int> &a, vector<int> &b) {
-    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
-    int n = 1;
-    while(n < a.size() + b.size())
-        n <<= 1;
-    fa.resize(n);
-    fb.resize(n);
-
-    fft(fa, 0);
-    fft(fb, 0);
-
-    for(int i = 0; i < n; i++)
-        fa[i] *= fb[i];
-    fft(fa, 1);
-    vector<int> res(n);
-    for(int i = 0; i < n; i++)
-        res[i] = round(fa[i].real());
+vd conv(const vd& a, const vd& b) {
+    if (a.empty() || b.empty()) return {};
+    vd res(sz(a) + sz(b) - 1);
+    int L = 32 - __builtin_clz(sz(res)), n = 1 << L;
+    vector<C> in(n), out(n);
+    copy(all(a), begin(in));
+    rep(i, 0, sz(b)) in[i].imag(b[i]);
+    fft(in);
+    for (C& x : in) x *= x;
+    rep(i, 0, n) out[i] = in[-i & (n - 1)] - conj(in[i]);
+    fft(out);
+    rep(i, 0, sz(res)) res[i] = imag(out[i]) / (4 * n);
     return res;
 }
+
 int main() {
     IO
     //Applications
