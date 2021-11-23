3a2dc3df7bfa0def6b51289bdc992f4880006506
Triplem5ds <mmmanany@yahoo.com>
2021-11-23 20:37:24 +0200

    Added second ACA

--- Strings/ACA.cpp
+++ Strings/ACA.cpp
@@ -1,44 +1,70 @@
-const int A = 26;
-int trie[N][A];
-int go[N][A];
-bool isLeaf[N]; ///this node is a string terminator;
-int fail[N];    ///the failure function for each
-void BFS() {
-    queue<int> q;
-    f(i,0,A) {
-        if(trie[0][i]) {
-            q.push(trie[0][i]);
-            fail[trie[0][i]] = 0;
+struct AC_FSM {
+#define ALPHABET_SIZE 26
+
+    struct Node {
+        int child[ALPHABET_SIZE], failure = 0, match_parent = -1;
+        vector<int> match;
+
+        Node() {
+            for (int i = 0; i < ALPHABET_SIZE; ++i)child[i] = -1;
         }
+    };
+
+    vector<Node> a;
+
+    AC_FSM() {
+        a.push_back(Node());
     }
 
-    while(q.size()) {
-        auto node = q.front();
-        q.pop();
-        for(int i = 0; i < A; i++) {
-            if(trie[node][i]) { ///calculate failure for you child
-                int to = trie[node][i];
-                int cur = fail[node]; ///int g = pi[i-1]
-                while(cur && !trie[cur][i]) ///while(g && s[g] != s[i])
-                    cur = fail[cur];    ///g = pi[g-1]
-                if(trie[cur][i])cur = trie[cur][i]; ///g += s[i] == s[g]
-                fail[to] = cur; ///pi[i] = g
-                q.push(to);
-                go[node][i] = trie[node][i]; ///
-            }   else {
-                go[node][i] = go[fail[node]][i]; ///note that the go of t
+    void construct_automaton(vector<string> &words) {
+        for (int w = 0, n = 0; w < words.size(); ++w, n = 0) {
+            for (int i = 0; i < words[w].size(); ++i) {
+                if (a[n].child[words[w][i] - 'a'] == -1) {
+                    a[n].child[words[w][i] - 'a'] = a.size();
+                    a.push_back(Node());
+                }
+                n = a[n].child[words[w][i] - 'a'];
+            }
+            a[n].match.push_back(w);
+        }
+        queue<int> q;
+        for (int k = 0; k < ALPHABET_SIZE; ++k) {
+            if (a[0].child[k] == -1) a[0].child[k] = 0;
+            else if (a[0].child[k] > 0) {
+                a[a[0].child[k]].failure = 0;
+                q.push(a[0].child[k]);
+            }
+        }
+        while (!q.empty()) {
+            int r = q.front();
+            q.pop();
+            for (int k = 0, arck; k < ALPHABET_SIZE; ++k) {
+                if ((arck = a[r].child[k]) != -1) {
+                    q.push(arck);
+                    int v = a[r].failure;
+                    while (a[v].child[k] == -1) v = a[v].failure;
+                    a[arck].failure = a[v].child[k];
+                    a[arck].match_parent = a[v].child[k];
+                    while (a[arck].match_parent != -1 &&
+                           a[a[arck].match_parent].match.empty())
+                        a[arck].match_parent =
+                                a[a[arck].match_parent].match_parent;
+                }
             }
         }
     }
 
-}
-void ins(string s) {
-    int cur = 0;
-    for(char c : s) {
-        if(!trie[cur][c - 'a']) {
-            trie[cur][c - 'a'] += 1;
+    void aho_corasick(string &sentence, vector<string> &words,
+                      vector<vector<int> > &matches) {
+        matches.assign(words.size(), vector<int>());
+        int state = 0, ss = 0;
+        for (int i = 0; i < sentence.length(); ++i, ss = state) {
+            while (a[ss].child[sentence[i] - 'a'] == -1)
+                ss = a[ss].failure;
+            state = a[state].child[sentence[i] - 'a'] = a[ss].child[sentence[i] - 'a'];
+            for (ss = state; ss != -1; ss = a[ss].match_parent)
+                for (int w: a[ss].match)
+                    matches[w].push_back(i + 1 - words[w].length());
         }
-        cur = trie[cur][c - 'a'];
     }
-    isLeaf[cur] = true;
-}
+};
