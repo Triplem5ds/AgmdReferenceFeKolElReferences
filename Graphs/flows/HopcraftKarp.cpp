cd8cafb3ed1638c51e33e47e42c0b724e2958393
Triplem5ds <mmmanany@yahoo.com>
2021-11-24 21:03:04 +0200

    Updated Hopcraft Karp

--- Graphs/flows/HopcraftKarp.cpp
+++ Graphs/flows/HopcraftKarp.cpp
@@ -1,75 +1,61 @@
-// Maximum bipartite matching
-// Index from 1
-// Find max independent set:
-// for(i = 1 → M) if (mat.matchL[i] > 0) {
-//   if (mat.dist[i] < inf) {
-//     for(j = 1 → N) if (ke[i][j]) right.erase(j); }
-//   else left.erase(i);
-// }
-// Find vertices that belong to all maximum matching:
-// - L = vertices not matched on left side --> BFS from these vertices
-//   (left --> right: unmatched edges, right --> left: matched edges)
-//   reachable vertices on left side --> not belong to some maximum matching
-// - Do similar for right side
-// Tested:
-// - http://codeforces.com/gym/100216 - J
-// - SRM 589 - 450
-// - http://codeforces.com/gym/100337 - A
-const int inf = 1000111;
-struct Matching {
-    int n;
-    vector<int> matchL, matchR, dist;
-    vector<bool> seen;
-    vector< vector<int> > ke;
+/**
+ * Author: Chen Xing
+ * Date: 2009-10-13
+ * License: CC0
+ * Source: N/A
+ * Description: Fast bipartite matching algorithm. Graph $g$ should be a list
+ * of neighbors of the left partition, and $btoa$ should be a vector full of
+ * -1's of the same size as the right partition. Returns the size of
+ * the matching. $btoa[i]$ will be the match for vertex $i$ on the right side,
+ * or $-1$ if it's not matched.
+ * Usage: vi btoa(m, -1); hopcroftKarp(g, btoa);
+ * Time: O(\sqrt{V}E)
+ * Status: stress-tested by MinimumVertexCover, and tested on oldkattis.adkbipmatch and SPOJ:MATCHING
+ */
+#pragma once
 
-    Matching(int n) : n(n), matchL(n+1), matchR(n+1), dist(n+1), seen(n+1, false), ke(n+1) {}
+bool dfs(int a, int L, vector<vi>& g, vi& btoa, vi& A, vi& B) {
+	if (A[a] != L) return 0;
+	A[a] = -1;
+	for (int b : g[a]) if (B[b] == L + 1) {
+		B[b] = 0;
+		if (btoa[b] == -1 || dfs(btoa[b], L + 1, g, btoa, A, B))
+			return btoa[b] = a, 1;
+	}
+	return 0;
+}
 
-    void addEdge(int u, int v) {
-        ke[u].push_back(v);
-    }
-
-    bool bfs() {
-        queue<int> qu;
-        for(int u = 1; u <= n; ++u)
-            if (!matchL[u]) {
-                dist[u] = 0;
-                qu.push(u);
-            } else dist[u] = inf;
-        dist[0] = inf;
-
-        while (!qu.empty()) {
-            int u = qu.front(); qu.pop();
-            for(__typeof(ke[u].begin()) v = ke[u].begin(); v != ke[u].end(); ++v) {
-                if (dist[matchR[*v]] == inf) {
-                    dist[matchR[*v]] = dist[u] + 1;
-                    qu.push(matchR[*v]);
-                }
-            }
-        }
-        return dist[0] != inf;
-    }
-
-    bool dfs(int u) {
-        if (u) {
-            for(__typeof(ke[u].begin()) v = ke[u].begin(); v != ke[u].end(); ++v)
-                if (dist[matchR[*v]] == dist[u] + 1 && dfs(matchR[*v])) {
-                    matchL[u] = *v;
-                    matchR[*v] = u;
-                    return true;
-                }
-            dist[u] = inf;
-            return false;
-        }
-        return true;
-    }
-
-    int match() {
-        int res = 0;
-        while (bfs()) {
-            for(int u = 1; u <= n; ++u)
-                if (!matchL[u])
-                    if (dfs(u)) ++res;
-        }
-        return res;
-    }
-};
+int hopcroftKarp(vector<vi>& g, vi& btoa) {
+	int res = 0;
+	vi A(g.size()), B(btoa.size()), cur, next;
+	for (;;) {
+		fill(all(A), 0);
+		fill(all(B), 0);
+		/// Find the starting nodes for BFS (i.e. layer 0).
+		cur.clear();
+		for (int a : btoa) if(a != -1) A[a] = -1;
+		rep(a,0,sz(g)) if(A[a] == 0) cur.push_back(a);
+		/// Find all layers using bfs.
+		for (int lay = 1;; lay++) {
+			bool islast = 0;
+			next.clear();
+			for (int a : cur) for (int b : g[a]) {
+				if (btoa[b] == -1) {
+					B[b] = lay;
+					islast = 1;
+				}
+				else if (btoa[b] != a && !B[b]) {
+					B[b] = lay;
+					next.push_back(btoa[b]);
+				}
+			}
+			if (islast) break;
+			if (next.empty()) return res;
+			for (int a : next) A[a] = lay;
+			cur.swap(next);
+		}
+		/// Use DFS to scan for augmenting paths.
+		rep(a,0,sz(g))
+			res += dfs(a, 0, g, btoa, A, B);
+	}
+}
