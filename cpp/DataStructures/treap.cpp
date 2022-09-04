9ddedeea66072388e82d0bbb36e7046decaed426
Triplem5ds <mmmanany@yahoo.com>
2022-09-04 07:56:28 +0200

    Treap

--- cpp/DataStructures/treap.cpp
+++ cpp/DataStructures/treap.cpp
@@ -1,79 +1,64 @@
-typedef struct item * pitem;
-struct item {
-    int prior, value, cnt;
-    bool rev;
-    pitem l, r;
-    item(int x, int y, int z){
-        value = x;
-        prior = y;
-        cnt = z;
-        rev = 0;
-        l = r = NULL;
-    }
+struct Node {
+    int key, pri = mrand(), sz = 1;
+    int lz = 0;
+    int idx;
+    array<Node*, 2> c = {NULL,NULL};
+    Node(int key, int idx) : key(key), idx(idx) {}
 };
-
-int cnt (pitem it) {
-    return it ? it->cnt : 0;
+int getsz(Node* t){
+    return t ? t->sz : 0;
 }
-
-void upd_cnt (pitem it) {
-    if (it)
-        it->cnt = cnt(it->l) + cnt(it->r) + 1;
+Node* calc(Node* t) {
+    t->sz = 1 + getsz(t->c[0]) + getsz(t->c[1]);
+    return t;
 }
-
-void push (pitem it) {
-    if (it && it->rev) {
-        it->rev = false;
-        swap (it->l, it->r);
-        if (it->l)  it->l->rev ^= true;
-        if (it->r)  it->r->rev ^= true;
+void prop(Node* cur) {
+    if(!cur || !cur->lz)
+        return;
+    cur->key += cur->lz;
+    if(cur->c[0])
+        cur->c[0]->lz += cur->lz;
+    if(cur->c[1])
+        cur->c[1]->lz += cur->lz;
+    cur->lz = 0;
+}
+array<Node*, 2> split(Node* t, int k) {
+    prop(t);
+    if(!t)
+        return {t, t};
+    if(getsz(t->c[0]) >= k) {   ///answer is in left node
+        auto ret = split(t->c[0], k);
+        t->c[0] = ret[1];
+        return {ret[0], calc(t)};
+    } else {  ///k > t->c[0]
+        auto ret = split(t->c[1], k - 1 - getsz(t->c[0]));
+        t->c[1] = ret[0];
+        return {calc(t), ret[1]};
     }
 }
-
-void merge (pitem & t, pitem l, pitem r) {
-    push (l);
-    push (r);
-    if (!l || !r)
-        t = l ? l : r;
-    else if (l->prior > r->prior)
-        merge (l->r, l->r, r),  t = l;
-    else
-        merge (r->l, l, r->l),  t = r;
-    upd_cnt (t);
+Node* merge(Node* u, Node* v) {
+    prop(u);
+    prop(v);
+    if(!u || !v)
+        return u ? u : v;
+    if(u->pri>v->pri) {
+        u->c[1] = merge(u->c[1], v);
+        return calc(u);
+    } else {
+        v->c[0] = merge(u, v->c[0]);
+        return calc(v);
+    }
 }
-
-void split (pitem t, pitem & l, pitem & r, int key, int add = 0) {
-    if (!t)
-        return void( l = r = 0 );
-    push (t);
-    int cur_key = add + cnt(t->l);
-    if (key <= cur_key)
-        split (t->l, l, t->l, key, add),  r = t;
-    else
-        split (t->r, t->r, r, key, add + 1 + cnt(t->l)),  l = t;
-    upd_cnt (t);
+int cnt(Node* cur, int x) {
+    prop(cur);
+    if(!cur)
+        return 0;
+    if(cur->key <= x)
+        return getsz(cur->c[0]) + 1 + cnt(cur->c[1], x);
+    return cnt(cur->c[0], x);
 }
-
-void reverse (pitem t, int l, int r) {
-    pitem t1, t2, t3;
-    split (t, t1, t2, l);
-    split (t2, t2, t3, r-l+1);
-    t2->rev ^= true;
-    merge (t, t1, t2);
-    merge (t, t, t3);
-}
-
-void output (pitem t) {
-    if (!t)  return;
-    push (t);
-    output (t->l);
-    printf ("%c", char(t->value));
-    output (t->r);
-}
-
-pitem gettreap(string s){
-        pitem ret=NULL;
-	int i;
-       for(i=0;i<s.size();i++)merge(ret,ret,new item(s[i],(rand()<<15)+rand(), 1));
-	return ret;
+Node* ins(Node* root, int val, int idx, int pos) {
+    auto splitted = split(root, pos);
+    root = merge(splitted[0], new Node(val, idx));
+    return merge(root, splitted[1]);
 }
